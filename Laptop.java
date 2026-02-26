0ebf653282ae22f720508afbe962072f04c23ce9
Fatima1515 <fatima.elhawi4@gmail.com>
2026-02-26 16:34:55 -0700

    Refactor Laptop class to extend Computer and improve structure

--- Laptop.java
+++ Laptop.java
@@ -1,33 +1,26 @@
-//Laptop computer: adds screen size to other Computer info
+// Laptop computer: adds screen size to other Computer info
 
-public class Laptop{
-    String screenSize=null;
-    Computer computer = null;  // Composition
-    
-    //Constructors
-    public Laptop() {} //No-arg constructor
+public class Laptop extends Computer {
 
+    private final String screenSize;
+
+    // Constructor
     public Laptop(String CPU, String RAM, String disk, String screenSize) {
-        //Composition from Computer superclass
-    	computer = new Computer(CPU, RAM, disk);
-        //Only in Laptop subclass
-        this.screenSize=screenSize;
+        super(CPU, RAM, disk);   // Call Computer constructor
+        this.screenSize = screenSize;
     }
 
-    //Setter
-    public void setScreenSize(String screenSize) {
-        this.screenSize=screenSize;
-    }
-
-    //Getter
+    // Getter
     public String getScreenSize() {
         return this.screenSize;
     }
 
-    //Return formatted version of data 
+    // Return formatted version of data
     @Override
     public String toString() {
-        return computer.toString() + "\tGPU:"  + "\tScreen:" + this.screenSize;
+        return "Type:Laptop\tCPU:" + getCPU() +
+               "\tRAM:" + getRAM() +
+               "\tDisk:" + getDisk() +
+               "\tScreen:" + this.screenSize;
     }
-    
 }
